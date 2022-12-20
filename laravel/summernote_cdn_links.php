a558265dd0a5e2ae0a1bb81f0aae042d97c9c2ca
rayan <rhrayan2228@gmail.com>
2022-12-20 11:10:17 +0600

    summernote cdn links

---
+++ laravel/summernote_cdn_links.php
@@ -0,0 +1,15 @@
+    <!-- Summernote CSS - CDN Link -->
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
+
+    <!-- Summernote JS - CDN Link -->
+    <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $("#summernote").summernote({
+                placeholder: 'describe your post',
+                height: 400,
+            });
+            $('.select_js').select2();
+        });
+    </script>
